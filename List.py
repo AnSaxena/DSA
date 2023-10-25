88f726a9566c919c6f29eec820f07793e87b9940
asaxena19 <122564201+AnSaxena@users.noreply.github.com>
2023-10-25 19:02:14 +0000

    finished

--- List.py
+++ List.py
@@ -13,6 +13,17 @@ l=[10,23,30,49]
 r=evenOdd(l)
 print (r)
 
+# Comprehension in python for above code
+def evenOdd(l):
+    e= [i for i in l if i%2==0]
+    o= [i for i in l if i%2!=0]
+    return (e,o)
+
+l=[10,11,12,13]
+r=evenOdd(l)
+print (r) 
+
+
 # Return a list of smaller element then the given element
 def getSmaller (l,x):
     s=[]
