c85d157a987bab8c3f55bd81db0a18e7abfecc79
asaxena19 <122564201+AnSaxena@users.noreply.github.com>
2023-10-25 20:31:50 +0000

    finish

--- List.py
+++ List.py
@@ -47,3 +47,47 @@ l=[10,20,3,4,5,6,22,111]
 x= 10
 r=getSmaller(l,x)
 print(r)
+
+# write own code to find largest no. in the list without using max function
+# O(n^2)
+def findLargest(l):
+    for i in l:
+        for j in l:
+            if j>i:
+             break
+        else:
+            return i
+l = [1,2,3,4,5,6,1,10,2]
+r=findLargest(l)
+print (r)
+
+#Above code in efficient manner
+
+def getlargest(l):
+    if not l:
+        return None
+    else:
+        res=l[0]
+        for i in range (1,len(l)):
+            if l[i]>res:
+                res=l[i]
+        return res
+
+# write own code to find second largest no. in the list 
+def getSmax(l):
+    if len(l)<=1:
+        return None
+    else:
+        m= max(l)
+        res=None
+        for i in l:
+            if i!=m:
+                if res == None:
+                    res=i
+                else:
+                    res = max(res,i)
+        return (res)
+
+l = [6,6,3,3]
+r=getSmax(l)
+print (r)
